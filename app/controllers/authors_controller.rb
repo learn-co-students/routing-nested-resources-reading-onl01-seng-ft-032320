a854caf9b7f3d860ec68b33dce7c8945ccb60381
jrestrepo922 <tinto922@gmail.com>
2020-06-06 07:49:27 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,17 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  # def posts_index
+  #   @author = Author.find_by_id(params[:id])
+  #   @posts = @author.posts
+  #   # being lazy and using the post folder views 
+  #   render template: 'posts/index'
+  # end
+
+  # def posts 
+  #   @author = Author.find_by_id(params[:id])
+  #   @post = @author.posts.find_by_id(params[:post_id])
+  #   # being lazy and using the post folder views 
+  #   render template: 'posts/show'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  
+  # Parent/child relationship.Author being parent and post being child(belongs to author)
+  resources :authors, only: [:show] do 
+    #nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
+  # deost the same thing
+  # get 'authors/:id/posts', to: "authors#posts_index"
+  # get  'authors/:id/posts/:post_id', to: "authors#posts"
+
+
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+
 
   root 'posts#index'
 
